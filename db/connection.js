8a610bfa3ebd1e807f87023fd41a1fd919d74161
Robertlh1 <robertlh1@DESKTOP-5JGUGOE.localdomain>
2022-04-05 20:14:52 -0700

    added open graph tags

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
   // user: 'robertlh1',
-  user: 'ubuntu'
+  user: 'ubuntu',
   host: 'localhost',
   database: 'ubuntu',
   password: 'yeeboy',
